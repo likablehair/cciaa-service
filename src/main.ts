05bec24beef47603733befbcd84722b85ff4aca1
filippopassalacqua <filippo.passalacqua.13@gmail.com>
2026-02-26 14:36:13 +0100

    STRAMO-217

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,2 @@
 export type { CompanySummary } from 'src/types/company.types';
-export { default as AIWSService } from 'src/clients/AIWSClient';
+export { default as AIWSClient } from 'src/clients/AIWSClient';
